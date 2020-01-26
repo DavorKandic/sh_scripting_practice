01a932300ffecb92eb9cbef8af8884e87b6c1fc6
Davor Kandic <markojank727@gmail.com>
2020-01-26 03:04:18 +0100

    learning takes time...

---
+++ another_simple_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+my_num=100
+
+if [ $my_num -eq 200 ]
+then
+	echo "$my_num is equal to 200"
+else
+	echo "$my_num is not equal to 200"
+fi
---
+++ simple_test.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+x=6
+y=2
+
+if [[ $x -gt $y ]];
+then
+	echo "$x is greater than $y"
+fi
+
