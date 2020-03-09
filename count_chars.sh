0db9948399dcbe9c8882d7baa223472b22dafa1d
Davor Kandic ( Vip mobile ) <rs1ohm@vipmobile.rs>
2020-03-09 14:43:02 +0100

    regular commit via script

--- count_chars.sh
+++ count_chars.sh
@@ -6,6 +6,7 @@
  total=$(cat $1 | wc -m)
  echo "Count of chars in $1:"
  chars=0
+ vocals=0
  for i in a b c d e f g h i j k l m n o p q r s t u v w x y z
  do 
 	 num=$(grep -o -i $i $1 | wc -l) 
@@ -13,6 +14,18 @@
 	 then
 	 	echo "$i - $num"
 	        chars=$(( $chars+$num ))
+		case $i in 
+			A|a|E|e|I|i|O|o|U|u)
+				vocals=$(( $vocals + $num ))
+				;;
+			*)
+				:
+				;;
+		esac
+
 	 fi
  done
+ cons=$(( $chars - $vocals ))
+ echo "Consonants: $cons"
+ echo "Vocals: $vocals"
  echo "Total: $chars"
